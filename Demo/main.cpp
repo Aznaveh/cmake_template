d6e688af2752233af939024a5d96dd5169196ae9
Mohsen Aznaveh <aznaveh@tamu.edu>
2021-05-01 15:37:52 -0500

    first commit

---
+++ Demo/main.cpp
@@ -0,0 +1,7 @@
+#include "header.hpp"
+int main()
+{
+    MyClass Obj;     // Create an object of MyClass
+    Obj.myMethod();  // Call the method
+    return 0;
+}
---
+++ Source/myMethod.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+#include "header.hpp"
+void MyClass::myMethod() { std::cout << "Hello World!\n"; }
